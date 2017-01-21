3520cadc5da9a9a852e31b7f0c6189240b08e003
DESKTOP-85V8C1V\Tommaso <tommaso.bassini@gmail.com>
2017-01-21 01:19:48 +0100

    PlayerStatus and wavecontroller

---
+++ Assets/PlayerStatus.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStatus : MonoBehaviour
+{
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+
+    void OnTriggerStay()
+    {
+        Debug.Log("suca");
+    }
+}
